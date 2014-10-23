ca83b453755df3e3c567153bef336b391d5d995d
Matthew Martin <matthewdeanmartin@gmail.com>
2014-10-23 18:41:15 -0400

    Templates as a concept
    Also thinking about how liskov substitution can be test so I can safely
    create class hierarchies

---
+++ BasicTypes/FundamentalTests/ListkovSubstitutionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BasicTypes.FundamentalTests
+{
+    [TestFixture]
+    public class ListkovSubstitutionTests
+    {
+        [Test]
+        public void ExerciseTests()
+        {
+            //Step 1, get list of all possible values of a type. Or a good selection including edge conditions.
+
+            //Step 2, write tests that use base class & verify behavior of base class
+
+            //Step 3, run same tests with derived objects.
+        }
+    }
+}
---
+++ BasicTypes/Parts/TemplatedPhrase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BasicTypes.Parts
+{
+    public class TemplatedPhrase
+    {
+        public TemplatedPhrase(string template, string glossTemplate)
+        {
+            
+        }
+
+
+
+    }
+
+    [TestFixture]
+    public class TemplatedPhraseTests
+    {
+        [Test]
+        public void TemplatedPhraseTests1()
+        {
+            string template = "{0} li pona {1}tawa {2}";
+            string corpusText = "ni li pona kin tawa mi";
+            string glossTemplate = "{2} {1} likes {0}";
+
+            string [] parts;
+            corpusText.TryExtract(template, out parts);
+            List<string> s = new List<string>();
+            foreach (string part in parts)
+            {
+                Word w = Words.Dictionary[part.Trim()];
+                string g= w.GlossMap["en"].First().Value.First();
+                s.Add(g);
+            }
+            Console.WriteLine(glossTemplate,s.ToArray());
+        }
+    }
+
+
+}
