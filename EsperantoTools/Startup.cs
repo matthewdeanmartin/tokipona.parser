197f338c1cb2fe26ff8be44299396ba00b4c116b
Matthew Martin <matthewdeanmartin@gmail.com>
2015-01-01 11:16:49 -0500

    Eo Tools- empty project

---
+++ EsperantoTools/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EsperantoTools.Startup))]
+namespace EsperantoTools
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
